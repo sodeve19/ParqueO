93cc865ff340edab1d34c7d9a28326c923bfe659
Sorel <sodeve619@gmail.com>
2015-01-20 10:42:03 -0500

    Add directions from where I am to the place I want to go and estimation of the taxi cost

--- app/assets/javascripts/parking_garages.js
+++ app/assets/javascripts/parking_garages.js
@@ -1,5 +1,6 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
+
 var map;
 var service;
 var infowindow;
@@ -8,7 +9,11 @@ var parking_garages_link;
 var markers = [];
 var place;
 
-$(document).on("ready page:load", function() {
+function ParkingLots(){
+
+//$(document).on("ready page:load", function() {
+
+  console.log("parking_garages java");
 
   map = new google.maps.Map(document.getElementById('map'), {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -192,9 +197,9 @@ $(document).on("ready page:load", function() {
   //     service.search(request, callback);
   //   };
   // }, "json");
-});
+//});
 
-
+}
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -238,6 +243,7 @@ function deleteMarkers() {
   console.log("markers =" + markers + "!!!");
   markers = [];
 }
+
 
 
 //google.maps.event.addDomListener(window, 'load', initialize);
--- app/assets/javascripts/taxi.js
+++ app/assets/javascripts/taxi.js
@@ -1,17 +1,33 @@
-$(document).on("ready page:load", function() {
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+var service;
+var infowindow;
+var myLatlng;
+var parking_garages_link;
+var markers = [];
+var place;
+var start;
+var end;
 
+//$(document).on("ready page:load", function() {
+function TaxiDirections(){
+
+  console.log("taxi js");
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
   map = new google.maps.Map(document.getElementById('map'), {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       zoom: 16
     });
-
+  directionsDisplay.setMap(map);
   if(navigator.geolocation) { 
 
     navigator.geolocation.getCurrentPosition(function(position) {
       myLatlng = new google.maps.LatLng(position.coords.latitude,
                                             position.coords.longitude);
-      parking_garages_link = "/parking_garages?latitude=" + position.coords.latitude + "&longitude=" + position.coords.longitude;
-  
+      start = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      
       var infowindow = new google.maps.InfoWindow({
         map: map,
         position: myLatlng,
@@ -20,9 +36,65 @@ $(document).on("ready page:load", function() {
 
       map.setCenter(myLatlng);
 
-      // ------- AÑADO GET
-      $.get("/parking_garages", { latitude: position.coords.latitude, longitude: position.coords.longitude } , function( data ) {
+      //-- Iba un GET
+
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesnt support Geolocation
+    handleNoGeolocation(false);
+  };
+
+  //--------- INPUT SEARCH INIT
+  var input = document.getElementById('pac-input');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      deleteMarkers();
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+      end = new google.maps.LatLng(place.geometry.location.k, place.geometry.location.D);
+      calcRoute();
+      
+      $.get("/parking_garages", { latitude: place.geometry.location.k, longitude: place.geometry.location.D } , function( data ) {
         console.log(data);
+
         for(var i = 0; i < data.length; i++) {
           console.log(data[i]);
           var ParLatlng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
@@ -44,17 +116,95 @@ $(document).on("ready page:load", function() {
           })(marker, i));
         };
       }, "json");
-      
-      // ------- FIN GET
 
+    }
 
+    map.fitBounds(bounds);
 
-    }, function() {
-      handleNoGeolocation(true);
+    var listener = google.maps.event.addListener(map, "idle", function() { 
+      if (map.getZoom() > 16) map.setZoom(16); 
+      google.maps.event.removeListener(listener); 
     });
-  } else {
-    // Browser doesnt support Geolocation
-    handleNoGeolocation(false);
+  });
+  // [END region_getplaces]
+
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+
+
+
+  //--------- END OF INPUT SEARCH
+
+  
+
+  var request = {
+    location: myLatlng,
+    radius: '500',
+    types: ['store']
+  }; 
+
+}
+
+
+function calcRoute() {
+  // var start = document.getElementById('start').value;
+  // var end = document.getElementById('end').value;
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
   };
 
-});
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+
+      $('#taxi').addClass('index-z');
+      // Display the distance:
+     document.getElementById('distance').innerHTML = 
+        "<strong>Distancia: </strong>" + Math.round(response.routes[0].legs[0].distance.value / 1000) + " km";
+
+     // Display the duration:
+     document.getElementById('duration').innerHTML = 
+        "<strong>Duración: </strong>" + Math.round(response.routes[0].legs[0].duration.value / 60 ) + " mins";
+
+      var time = (response.routes[0].legs[0].duration.value / 60 );
+      var distance = (response.routes[0].legs[0].distance.value / 78);
+
+      var costo = (2.700 + (distance * 83));
+
+      if (costo < 4700) {
+        costo = 4700;
+      }
+
+      document.getElementById('taxi-dis').innerHTML =
+        "<strong>Costo del taxi: </strong>" + Math.round(costo) + " pesos";
+
+
+      directionsDisplay.setDirections(response);
+    } else {
+      console.log(status);
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
